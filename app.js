d03386240aa93330e4184a018b847b0849f80410
kailash360 <kailashkejriwal21@gmail.com>
2021-09-03 18:03:39 +0530

    integrated routes for shows

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const auth = require("./src/routes/auth");
 const api = require("./src/routes/api");
 
 const app = express();
+app.use(express.json());
 const port = 4000;
 
 //Route to fetch data
 app.use("/api", api);
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Server running at port ${port}`);
+    console.log(`Server running at port ${port}`);
 });
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,9 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const { addEvent, getEvents } = require("../controllers/events")
+const { getShow, addShow } = require("../controllers/shows")
 
 //Redirect to events section
-router.post("/events", addEvent);
 router.get("/events", getEvents);
+router.post("/events", addEvent);
+
+//Redirect to shows section
+router.get("/shows", getShow);
+router.post("/shows", addShow);
 
 module.exports = router;
