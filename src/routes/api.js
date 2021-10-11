6f1d9b644f9d2c5b2494cf8045f3c8bd3a8a8070
kailash360 <kailashkejriwal21@gmail.com>
2021-10-11 16:44:44 +0530

    added controller and routes for teams

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,19 +5,28 @@ const { addEvent, getEvents } = require("../controllers/events")
 const { getShow, addShow } = require("../controllers/shows")
 const { getArticles } = require('../controllers/articles')
 const { getInterviews } = require('../controllers/interviews')
+const { getTeams, addTeamMember, updateTeamMember } = require('../controllers/team')
 
 //Redirect to events section
-router.get("/events", getEvents);
-router.post("/events", addEvent);
+router
+    .get("/events", getEvents)
+    .post("/events", addEvent);
 
 //Redirect to shows section
-router.get("/shows", getShow);
-router.post("/shows", addShow);
+router
+    .get("/shows", getShow)
+    .post("/shows", addShow);
 
 //Redirect to articles section
 router.get("/articles", getArticles);
 
 //Redirect to articles section
 router.get("/interviews", getInterviews);
+
+//Redirect to teams section
+router
+    .get("/teams", getTeams)
+    .post("/teams", addTeamMember)
+    .patch("/teams/:rollNumber", updateTeamMember)
 
 module.exports = router;
