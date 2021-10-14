a016effa4e4f428aeaeec2c319658f878ffec757
Raj <varasaniraj5@gmail.com>
2021-10-14 19:30:57 +0530

    Set up cors

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionSuccessStatus: 200,
+};
+
 // initial code
 require("dotenv").config();
 const express = require("express");
@@ -10,12 +16,12 @@ const auth = require("./src/routes/auth");
 const api = require("./src/routes/api");
 
 const app = express();
-app.use(express.json());
+app.use(cors(corsOptions));
 const port = 4000;
 
 //Route to fetch data
 app.use("/api", api);
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running at port ${port}`);
 });
