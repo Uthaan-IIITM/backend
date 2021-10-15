5f752b21043f9998033eda9414a2e7e331a43543
Raj <varasaniraj5@gmail.com>
2021-10-15 21:29:07 +0530

    Updated shows schema and APIs

--- src/controllers/shows.js
+++ src/controllers/shows.js
@@ -1,62 +1,48 @@
-const shows = require('../models/showsModel.js')
+const shows = require("../models/showsModel.js");
 
 //For fetching the show
-exports.getShow = async(req, res) => {
-
-    //Extracting name of the show and season
-    const queryString = new URLSearchParams(req.query)
-    const name = queryString.get("name")
-    const season = parseInt(queryString.get("season"))
-
-    //Find the show in the database and take the url 
-    try {
-        await shows.findOne({ name: name, season: season })
-            .then((result, err) => {
-                if (err) {
-                    console.log(err)
-                    res.send({ status: 404, message: err.message })
-                } else {
-                    const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${process.env.GOOGLE_API_KEY}&playlistId=${result.playlistId}&part=snippet&maxResults=50`
-                    res.send({ status: 200, url: url })
-                }
-            })
-    } catch (err) {
-        console.log(err)
-        res.send({ status: 404, message: err.message })
-    }
-}
+exports.getShow = async (req, res) => {
+  //Find the show in the database and take the url
+  try {
+    let dataReceived = await shows.find({}, { _id: 0, __v: 0 });
+    res.send({ status: 200, data: dataReceived });
+  } catch (err) {
+    console.log(err);
+    res.send({ status: 404, message: err.message });
+  }
+};
 
 //For adding a new show to the database
-exports.addShow = async(req, res) => {
-    console.log(req.body)
+exports.addShow = async (req, res) => {
+  //Extract the fields from the request body
+  const name = req.body.name;
+  const playlistIDs = req.body.playlistIDs;
 
-    //Extract the fields from the request body
-    const name = req.body.name
-    const season = parseInt(req.body.season)
-    const playlistId = req.body.playlistId
+  //Check for field validation
+  if (!name || !playlistIDs) {
+    return res.send({ status: 400, message: "Fields cannot be empty" });
+  }
 
-    //Check for field validation
-    if (!name || !season || !playlistId) {
-        res.send({ status: 400, message: "Fields cannot be empty" })
-    }
+  //Create an instance and save into database
+  const newShow = new shows({
+    name: name,
+    playlistIDs: playlistIDs,
+  });
 
-    //Create an instance and save into database
-    const newShow = new shows({
-        name: name,
-        season: season,
-        playlistId: playlistId
-    })
-
-    try {
-        newShow.save((err, result) => {
-            if (err) {
-                console.log(err)
-                res.send({ status: 500, message: err.message })
-            }
-            res.send({ status: 200, message: "show added successfully", data: result })
-        })
-    } catch (err) {
-        console.log(err)
-        res.send({ status: 404, message: err.message })
-    }
-}
+  try {
+    newShow.save((err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({ status: 500, message: err.message });
+      }
+      res.send({
+        status: 200,
+        message: "show added successfully",
+        data: result,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send({ status: 404, message: err.message });
+  }
+};
